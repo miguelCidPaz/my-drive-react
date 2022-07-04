f085a991100210c2d92cb9cf14e1e3f332611f4d
Miguel Cid <miguel.cid.paz@gmail.com>
2022-07-04 11:54:58 +0200

    buttons taskbar

--- src/components/Buttons/MenuButton.jsx
+++ src/components/Buttons/MenuButton.jsx
@@ -1,16 +1,19 @@
+
 export const MenuButton = ({ theme, Symbol, info, type }) => {
     return (
         type === 'search' ?
             <div className="search--main">
-                <input type="text" className="search--input" />
+                <input type="text" className="search--input" placeholder="Escribe aqui para buscar" />
                 <button className={`buttons--general ${theme}`}>
+                    <div className="buttons--help">{info}</div>
                     <Symbol className={`buttons--symbol ${theme}`} />
                 </button>
             </div>
             :
             <button
                 className={`buttons--general ${theme} ${type === 'main' ? 'mainbutton--main' : 'microbutton--main'}`}>
-                <Symbol className={`buttons--symbol ${theme}`} />
+                <div className="buttons--help">{info}</div>
+                <Symbol className={`buttons--symbol ${theme} `} />
             </button>
     )
 }
